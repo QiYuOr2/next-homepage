eba993c8542ecc7eaee40675c441abebcd0c677e
Tuzilow <1176281967@qq.com>
2020-11-23 17:16:02 +0800

    添加备案信息

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -13,13 +13,22 @@ export default function Layout({ children }: Props) {
       <Header />
       <main className={styles.main}>{children}</main>
       <footer className={styles.footer}>
-        <span>Copyright © 2016-2020 BAR Team. All rights reserved.</span>
-        <span>
+        <p>
+          <a href="https://beian.miit.gov.cn/">
+            ICP 备案号： 豫ICP备18029085号-1
+          </a>
+        </p>
+        <p className={styles.contact}>
+          <span>通信地址：河南省安阳市文峰区文明大道265号图书馆402房</span>
+          <span>邮箱：1176281967@qq.com</span>
+        </p>
+        <p>Copyright © 2016-2020 BAR Team. All rights reserved.</p>
+        <p>
           Power by
           <a className={styles.powerlink} href="https://www.nextjs.cn/">
             Next.js
           </a>
-        </span>
+        </p>
       </footer>
     </div>
   );
